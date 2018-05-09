a82de1a72692ba8ede773188836b7c5471de6cbb
autopsia <32441534+autopsia@users.noreply.github.com>
2018-05-08 23:50:44 -0500

    w

--- jsframeworks/js/master.js
+++ jsframeworks/js/master.js
@@ -154,7 +154,7 @@ function calcPageFillRadius(x, y) {
 
 function addClickListeners() {
   document.addEventListener("touchstart", handleEvent);
-  document.addEventListener("mousedown", handleEvent);
+  document.addEventListener("dblclick", handleEvent);
 };
 
 function handleEvent(e) {
@@ -304,11 +304,11 @@ function handleInactiveUser() {
   
   function clearInactiveTimeout() {
     clearTimeout(inactive);
-    document.removeEventListener("mousedown", clearInactiveTimeout);
+    document.removeEventListener("dblclick", clearInactiveTimeout);
     document.removeEventListener("touchstart", clearInactiveTimeout);
   }
   
-  document.addEventListener("mousedown", clearInactiveTimeout);
+  document.addEventListener("dblclick", clearInactiveTimeout);
   document.addEventListener("touchstart", clearInactiveTimeout);
 }
 
@@ -320,8 +320,49 @@ function startFauxClicking() {
 }
 
 function fauxClick(x, y) {
-  var fauxClick = new Event("mousedown");
+  var fauxClick = new Event("dblclick");
   fauxClick.pageX = x;
   fauxClick.pageY = y;
   document.dispatchEvent(fauxClick);
 }
+// target elements with the "draggable" class
+interact('.draggable')
+  .draggable({
+    // enable inertial throwing
+    inertia: true,
+    // keep the element within the area of it's parent
+    // enable autoScroll
+    autoScroll: true,
+
+    // call this function on every dragmove event
+    onmove: dragMoveListener,
+    // call this function on every dragend event
+    onend: function (event) {
+      var textEl = event.target.querySelector('p');
+
+      textEl && (textEl.textContent =
+        'moved a distance of '
+        + (Math.sqrt(Math.pow(event.pageX - event.x0, 2) +
+                     Math.pow(event.pageY - event.y0, 2) | 0))
+            .toFixed(2) + 'px');
+    }
+  });
+
+  function dragMoveListener (event) {
+    var target = event.target,
+        // keep the dragged position in the data-x/data-y attributes
+        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+    // translate the element
+    target.style.webkitTransform =
+    target.style.transform =
+      'translate(' + x + 'px, ' + y + 'px)';
+
+    // update the posiion attributes
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+  }
+
+  // this is used later in the resizing and gesture demos
+  window.dragMoveListener = dragMoveListener;
