109fd1b840eb51a138270b962e2926dfe11e315a
autopsia <32441534+autopsia@users.noreply.github.com>
2019-07-01 08:14:18 -0500

    Update script.js

--- reactjs/script.js
+++ reactjs/script.js
@@ -1,4 +1,4 @@
-// COMPONENTS
+        // COMPONENTS
 // ----------
 // Main
 //   UI
@@ -7,10 +7,10 @@
 //       ProductItem
 
 // Components in reverse order of above
-const ProductItem = ({ category, name }) => (
-  <div className="category__list-item boxy flex-spread">
-    {name}
-    <div className={`category--${category} circle`} />
+const ProductItem = ({ categoria, nombre }) => (
+  <div className="categoria__list-item boxy flex-spread">
+    {nombre}
+    <div className={`categoria--${categoria} circle`} />
   </div>
 );
 
@@ -18,25 +18,25 @@ const ProductItems = ({ state: { products, displayCategory } }) => (
   <div>
     {products
       .filter(
-        ({ category }) =>
-          displayCategory === category || displayCategory === "Todos"
+        ({ categoria }) =>
+          displayCategory === categoria || displayCategory === "Todos"
       )
-      .map(({ category, name }) => (
-        <ProductItem key={`ProductItems-name`} category={category} name={name} />
+      .map(({ categoria, nombre }) => (
+        <ProductItem key={`ProductItems-nombre`} categoria={categoria} nombre={nombre} />
       ))}
   </div>
 );
 
-const ButtonCategory = ({ setCategory, category }) => (
-  <button className={`btn-${category} button is-fullwidth`} onClick={() => setCategory(category)}>
-    {category}
+const ButtonCategory = ({ setCategory, categoria }) => (
+  <button className={`btn-${categoria} button is-fullwidth`} onClick={() => setCategory(categoria)}>
+    {categoria}
   </button>
 );
 
 
 const ButtonCategories = (productCategories, setCategory) => (
-  productCategories.map((category, i) => (
-    <ButtonCategory key={category} setCategory={setCategory} category={category} />
+  productCategories.map((categoria, i) => (
+    <ButtonCategory key={categoria} setCategory={setCategory} categoria={categoria} />
   ))
 );
 
@@ -57,83 +57,52 @@ const UI = ({
       </div>
     </div>
   );
-
+  const PRODUCTS = [];
 class Main extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       displayCategory: "Todos",
+      PRODUCTS: [],
       products: PRODUCTS,
-      productCategories: PRODUCT_CATEGORIES
+      productCategories: PRODUCT_CATEGORIES,
+      
     };
     this.setCategory = this.setCategory.bind(this);
-  }
-  setCategory(category) {
+  };
+  setCategory(categoria) {
     this.setState({
-      displayCategory: category
+      displayCategory: categoria
     });
   }
+  
+
   render() {
     return <UI setCategory={this.setCategory} state={this.state} />;
   }
+
 }
 
 // data
-const PRODUCTS = [
-  { category: "Entrada", name: "Causa rellena" },
-  { category: "Entrada", name: "Ceviche" },
-  { category: "Entrada", name: "Tamal" },
-  { category: "Entrada", name: "Alitas Broster" },
-  { category: "Entrada", name: "Tequenos" },
-  { category: "Entrada", name: "Papa a la Huancaina" },
-  { category: "Sopa", name: "Sopa a la minuta" },
-  { category: "Sopa", name: "Menestron" },
-  { category: "Sopa", name: "Shambar" },
-  { category: "Sopa", name: "Sopa de letras" },
-  { category: "Sopa", name: "Sopa de leche" },
-  { category: "Sopa", name: "Caldo de gallina" },
-  { category: "Sopa", name: "Caldo de cabeza" },
-  { category: "Sopa", name: "Patasca" },
-  { category: "Segundo", name: "Arroz con pollo" },
-  { category: "Segundo", name: "Frejoles con seco de pollo" },
-  { category: "Segundo", name: "Estofado de pollo" },
-  { category: "Segundo", name: "Aji de gallina" },
-  { category: "Segundo", name: "Arroz a la cubana" },
-  { category: "Segundo", name: "Carapulcra" },
-  { category: "Segundo", name: "Chicharron de pescado" },
-  { category: "Segundo", name: "Cau-cau" },
-  { category: "Postre", name: "Mazamorra" },
-  { category: "Postre", name: "Arroz con Leche" },
-  { category: "Postre", name: "Shampu" },
-  { category: "Postre", name: "Picarones" },
-  { category: "Postre", name: "Flan" },
-  { category: "Postre", name: "Tres leches" },
-  { category: "Postre", name: "Suspiro a la limena" },
-  { category: "Bebida", name: "Agua" },
-  { category: "Bebida", name: "Gaseosa" },
-  { category: "Bebida", name: "Limonada" },
-  { category: "Bebida", name: "Te" },
-  { category: "Bebida", name: "Cafe" },
-  { category: "Bebida", name: "Chicha morada" },
-  { category: "Bebida", name: "Juego de Naranja" },
-  { category: "Bebida", name: "Agua de coco" },
-  { category: "Bebida", name: "Anis" },
-  { category: "Bebida", name: "Chicha de jora" },
-];
 
-componentDidMount(){
-fetch('http://pix.pe/servicioandroid/serviciocategorias').then(response => response.json())
-.then(PRODUCTS => this.setState({PRODUCTS}))
-.catch(e => console.log(e));
-}
+  fetch('http://pix.pe/servicioandroid/serviciocategorias.php').then(response => response.json())
+  .then(PRODUCTS => PRODUCTS)
+  .catch(e => console.log(e));
+  
 
-// get unique category items
+
+
+// get unique categoria items
 const uniqueItems = (x, i, a) => a.indexOf(x) === i;
-const PRODUCT_CATEGORIES = PRODUCTS.map(prod => prod.category).filter(
+const PRODUCT_CATEGORIES = PRODUCTS.map(prod => prod.categoria).filter(
   uniqueItems
 );
+
 
 PRODUCT_CATEGORIES.push("Todos");
 PRODUCT_CATEGORIES.sort();
 
 ReactDOM.render(<Main products={PRODUCTS} />, document.getElementById("root"));
+       
+
+       
